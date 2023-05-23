61fad9d950ea48304d6add0cedf2d1f60269917d
ThyLuu <luuthythy2003@gmail.com>
2023-05-23 21:49:29 +0700

    23/5

---
+++ VisualCode/DangNhap/DangNhap.js
@@ -0,0 +1,9 @@
+function showPassFuntion() {
+    var x = document.getElementById("myPass");
+    if (x.type === "password") {
+      x.type = "text";
+    } 
+    else {
+      x.type = "password";
+    }
+  }
